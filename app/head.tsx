1acc95bfa6d8c24e57d82b6b8d2b59221d026629
MiguelOlarte06 <miguelolarte.2606@gmail.com>
2023-02-24 15:20:33 -0500

    Subiendo algunos cambos

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>SYGESCOL TYPE</title>
+      <title>SYGESCOL TYPESCRIPT</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/LogoSYGESCOL2022.png" />
     </>
