1f58d87725b984d6ce1a56be8335c12acd4d1d02
MiguelOlarte06 <miguelolarte.2606@gmail.com>
2023-03-02 18:09:33 -0500

    vista para planilla descriptores y añadiendo menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,37 @@
-"use client"
+"use client";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import "../styles/globals.css";
+import Header from "./Header";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  const data: any = useSearchParams()
+  const data: any = useSearchParams();
   const getDatos = async () => {
-    await axios.post("/api/Login/BaseInfoUser", {
-      docente: data.get("d"),
-      colegio: data.get("c")
-    }).then((res) => {
-      if (res.status == 200) {
-        localStorage.setItem("datosColegio", JSON.stringify(res?.data?.colegio || []))
-      }
-    })
-
-  }
+    await axios
+      .post("/api/Login/BaseInfoUser", {
+        docente: data.get("d"),
+        colegio: data.get("c"),
+      })
+      .then((res) => {
+        if (res.status == 200) {
+          localStorage.setItem(
+            "datosColegio",
+            JSON.stringify(res?.data?.colegio || [])
+          );
+        }
+      });
+  };
   useEffect(() => {
     getDatos();
-  }, [])
+  }, []);
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <Header>{children}</Header>
     </html>
-  )
+  );
 }
