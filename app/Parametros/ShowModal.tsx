243e122f0ce5dc78ba3179334156ce88ffc02031
pipe5999 <carvajal5999@gmail.com>
2023-03-08 18:04:03 -0500

    Subida de las 6

--- app/Parametros/ShowModal.tsx
+++ app/Parametros/ShowModal.tsx
@@ -28,12 +28,7 @@ export default function ShowModal({
         <div className="overflow-auto max-h-screen p-3">
           <div className="rounded-lg border border-gray-400 shadow-md m-5">
             <div className="flex justify-center  text-gray-300  bg-cyan-900 border-solid rounded-md text-md font-bold   px-4 py-1">
-              <h1
-                onClick={() => {
-                  setVisible(false);
-                }}
-                className="fond-bold text-lg text-center "
-              >
+              <h1 className="fond-bold text-lg text-center ">
                 {ParametroCliked.Title}
               </h1>
             </div>
@@ -90,6 +85,7 @@ export default function ShowModal({
                               width="2.0em"
                               xmlns="http://www.w3.org/2000/svg"
                             >
+                              <title>Definción de Parámetros</title>
                               <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h360c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H184V184h656v320c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8V144c0-17.7-14.3-32-32-32zM653.3 599.4l52.2-52.2a8.01 8.01 0 0 0-4.7-13.6l-179.4-21c-5.1-.6-9.5 3.7-8.9 8.9l21 179.4c.8 6.6 8.9 9.4 13.6 4.7l52.4-52.4 256.2 256.2c3.1 3.1 8.2 3.1 11.3 0l42.4-42.4c3.1-3.1 3.1-8.2 0-11.3L653.3 599.4z"></path>
                             </svg>
                             <svg
@@ -102,6 +98,7 @@ export default function ShowModal({
                               width="2.0em"
                               xmlns="http://www.w3.org/2000/svg"
                             >
+                              <title>Abrir Parámetro</title>
                               <path
                                 className="stroke-black hover:stroke-green-500"
                                 fill="none"
@@ -119,7 +116,7 @@ export default function ShowModal({
             </table>
             <div className="flex items-center justify-center py-1 bg-gray-200">
               <button
-                onAuxClick={() => {
+                onClick={() => {
                   setVisible(false);
                 }}
                 className=" w28 rounded-full bg-cyan-900 p-2 px-4 text-white hover:bg-red-600 "
