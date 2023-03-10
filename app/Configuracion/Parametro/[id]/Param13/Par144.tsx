89702cc98f3944c2f41f5f90761eca777f544027
pipe5999 <carvajal5999@gmail.com>
2023-03-10 08:53:14 -0500

    cambios rutas

--- app/Configuracion/Parametro/[id]/Param13/Par144.tsx
+++ app/Configuracion/Parametro/[id]/Param13/Par144.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import getDataParametro from "../../../../../utils/GetParametro";
 
 export default function Par144() {
-  return <div>Par144</div>;
+  const [data, setData] = React.useState({} as any);
+
+  console.log(data);
+
+  useEffect(() => {
+    const GetInfo = async () => {
+      const resultado = await getDataParametro(144, 2);
+      setData(resultado);
+    };
+    GetInfo();
+  }, []);
+
+  return (
+    <div>
+      {/* <Parametro1 />
+      <CardsPreguntas /> */}
+      {data?.infoParametros?.conf_nom_ver}
+    </div>
+  );
 }
--- app/Configuracion/Parametro/[id]/Param7/Par119.tsx
+++ app/Configuracion/Parametro/[id]/Param7/Par119.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import getDataParametro from "../../../../../utils/GetParametro";
 
 export default function Par119() {
-  return <div>Par119</div>;
+  const [data, setData] = React.useState({} as any);
+
+  console.log(data);
+
+  useEffect(() => {
+    const GetInfo = async () => {
+      const resultado = await getDataParametro(119, 2);
+      setData(resultado);
+    };
+    GetInfo();
+  }, []);
+
+  return (
+    <div>
+      {/* <Parametro1 />
+      <CardsPreguntas /> */}
+      {data?.infoParametros?.conf_nom_ver}
+    </div>
+  );
 }
