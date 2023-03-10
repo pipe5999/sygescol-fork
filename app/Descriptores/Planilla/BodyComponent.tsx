ea235884002d1b50b20dfaaf9bd8acf7d4052142
brahian351 <brahiangamer@gmail.com>
2023-03-10 11:40:36 -0500

    creo que lo logre

--- app/Descriptores/Planilla/BodyComponent.tsx
+++ app/Descriptores/Planilla/BodyComponent.tsx
@@ -1,11 +1,39 @@
 "use client";
 import axios from "axios";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import DimensionAbreviatura from "./DimensionAbreviatura";
+import Registro from "./ViewsPlanilla/Registro";
 
 function BodyComponent() {
+  const nacional = [
+    {
+      id: 1,
+      img: "caraSuperior",
+      title: "Superior",
+    },
+    {
+      id: 2,
+      img: "caraAlto",
+      title: "Alto",
+    },
+    {
+      id: 3,
+      img: "caraBasico",
+      title: "Básico",
+    },
+    {
+      id: 4,
+      img: "caraBajo",
+      title: "Bajo",
+    },
+  ];
+  const [registro, setRegistro] = useState(false);
   const [datoDimensiones, setDatos] = useState({} as any);
+  const [estudent, setEstudent] = useState([] as any);
+  const [dataEnvia, setDataEnvia] = useState({} as any);
   const [open, setOpen] = useState(null as any);
+  const [escala, setEscala] = useState([] as any);
   const [data, setData] = useState({} as any);
   const [dimension, setDimension] = useState({} as any);
   const GetInfoBase = async () => {
@@ -31,11 +59,23 @@ function BodyComponent() {
       info: info,
     });
   };
+  const openRegistro = (estudiante: any, desempeño: any) => {
+    setDataEnvia({ Desempeño: desempeño, Estudiante: estudiante });
+    setRegistro(true);
+  };
   useEffect(() => {
     GetInfoBase();
   }, []);
   return (
     <>
+      {registro && (
+        <Registro
+          cga={dimension.CgaId}
+          escala={dataEnvia.Desempeño}
+          estudiante={dataEnvia.Estudiante}
+          show={setRegistro}
+        />
+      )}
       <div className="uppercase text-center font-bold lg:text-2xl py-4 bg-blue-900 text-white rounded-b-[1.6rem]">
         Planilla de Calificaciones para el Nivel Escolar Preescolar
       </div>
@@ -58,14 +98,14 @@ function BodyComponent() {
           {(dimension && dimension?.nombreAsigna) ||
             "No se ha seleccionado una dimensión"}
         </div>
-        <div className="rounded-lg border-2 border-blue-600 text-center w-full py-2 px-4">
-          #Periodo Académico#
+        <div className="rounded-lg border-2 border-blue-600 text-center w-full py-2 px-4 font-bold">
+          {(data && data?.info?.periodo?.per_nombre) || ""}
         </div>
         <div className="rounded-lg border-2 border-blue-600 text-center w-full py-2 px-4 font-bold">
           Grupo: {data && data?.grupo?.grupo_nombre}
         </div>
       </div>
-      <div className="grid grid-cols-5 ">
+      <div className="grid grid-cols-5 mt-4">
         <div className="bg-blue-300 text-center">
           <h1 className="p-2 font-bold">Foto</h1>
         </div>
@@ -82,13 +122,61 @@ function BodyComponent() {
           <h1 className="p-2 font-bold">Consulta del Registro Efectuado</h1>
         </div>
         {data?.info &&
-          data?.info?.alumnos?.map((inf: any) => (
+          data?.info?.alumnos?.map((inf: any, key: number) => (
             <>
               <div></div>
               <div className="p-2 font-bold text-center">{inf?.codigo}</div>
               <div className="p-2 font-bold">{inf?.nombre}</div>
-              <div></div>
-              <div></div>
+              <div className="p-2 font-bold">
+                <div className="flex flex-row justify-between my-2 gap-2 lg:gap-0.5">
+                  {nacional.map((nac) => {
+                    return (
+                      <div
+                        className={`rounded-lg cursor-pointer ${
+                          escala[key] == nac?.id && nac?.id < 3
+                            ? "bg-green-600"
+                            : escala[key] == nac?.id && nac?.id > 2
+                            ? "bg-red-600"
+                            : "bg-blue-100"
+                        } 2xl:p-2 p-0`}
+                        onClick={() => {
+                          if (dimension?.CgaId) {
+                            const dat = [...escala];
+                            const newEst = [...estudent];
+                            newEst[key] = inf.matricula;
+                            dat[key] = nac?.id;
+                            setEscala(dat);
+                            setEstudent(newEst);
+                          }
+                        }}
+                      >
+                        <h1 className="lg:text-base font-medium">
+                          {nac.title}
+                        </h1>
+                        <Image
+                          width={50}
+                          height={50}
+                          src={`/Descriptores/${nac.img}.png`}
+                          alt="Imagen Desempeño"
+                        />
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="p-2 text-center flex items-center justify-center">
+                <button
+                  className="p-2 text-center font-bold bg-blue-600 text-white rounded-md hover:bg-blue-400"
+                  disabled={estudent.indexOf(inf.matricula) >= 0 ? false : true}
+                  onClick={() => {
+                    if (estudent.indexOf(inf.matricula) >= 0) {
+                      openRegistro(inf.matricula, escala[key]);
+                    }
+                  }}
+                >
+                  VER REGISTRO
+                </button>
+              </div>
             </>
           ))}
       </div>
