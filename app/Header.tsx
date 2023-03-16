1111c14b440b76c134d29ae2ff5afc9eff481dd0
brahian351 <brahiangamer@gmail.com>
2023-03-16 11:18:27 -0500

    cambios final

--- app/Header.tsx
+++ app/Header.tsx
@@ -102,6 +102,26 @@ const Header = ({ children }: Props) => {
               }
               submenu="Planilla Descriptores"
             />
+            <MenuItem
+              link="/PDF/Preescolar"
+              icono={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                  />
+                </svg>
+              }
+              submenu="Boletín Preescolar"
+            />
             <div
               className="cursor-pointer border-2 border-blue-600 p-2 hover:bg-blue-600 hover:border-l-4 hover:border-l-white text-white rounded flex items-center gap-2 transition-all duration-200"
               onClick={() => {
