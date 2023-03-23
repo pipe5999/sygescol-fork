91b9abbced28faa187379420810e26d2ee9b3fdb
jhonned01 <jhonned01@gmail.com>
2023-03-23 09:30:24 -0500

    antes de brayan

---
+++ app/api/CierrePeriodo/Docentes/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: any, { params }: any) {
+  const { colegio, grupos } = await req.json();
+
+  const GetConfiguracion = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/CierrePeriodo/Params/${colegio?.value}`
+  ).then((res) => res?.json());
+
+  console.log(GetConfiguracion);
+
+  //   const gruposPorId = grupos?.reduce((acc: any, el: any) => {
+  //     let key = el.GrupoId;
+
+  //     if (!acc[key]) {
+  //       acc[key] = [];
+  //     }
+
+  //     acc[key];
+
+  //     return acc;
+  //   }, {});
+
+  //   let sqlBaseGruposDcne = `SELECT * FROM cga INNER JOIN dcne ON dcne.i=cga.g INNER JOIN v_grupos ON v_grupos.grupo_id=cga.b WHERE v_grupos.grupo_id in (${Object.keys(
+  //     gruposPorId
+  //   ).join(",")})`;
+
+  //   console.log(sqlBaseGruposDcne);
+
+  return NextResponse.json(
+    { GruposCerrar: [] },
+    {
+      status: 200,
+    }
+  );
+}
--- app/api/CierrePeriodo/cron/route.ts
+++ app/api/CierrePeriodo/cron/route.ts
@@ -2,39 +2,58 @@ import { NextResponse } from "next/server";
 import School from "../../../../utils/School";
 
 export async function GET() {
-  const colegios = School();
+  try {
+    const colegios = School();
 
-  if (colegios.length > 0) {
-    //   console.log("ejecute el cron");
-    let cont = 0;
-    colegios.forEach(async (colegio, key) => {
-      const DateCierrePeriodo = await fetch(
-        `${process.env.NEXT_PUBLIC_APP_URL}/api/CierrePeriodo/VerificarFechas/${key}`
-      ).then((res) => res.json());
-
-      if (DateCierrePeriodo?.GruposCerrar?.length) {
-        const DateCierreConfig = await fetch(
-          `${process.env.NEXT_PUBLIC_APP_URL}/api/CierrePeriodo/Students`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              colegio: colegio,
-              grupos: DateCierrePeriodo?.GruposCerrar,
-            }),
-          }
+    if (colegios.length > 0) {
+      colegios.forEach(async (colegio, key) => {
+        const DateCierrePeriodo = await fetch(
+          `${process.env.NEXT_PUBLIC_APP_URL}/api/CierrePeriodo/VerificarFechas/${key}`
         ).then((res) => res.json());
-        // console.log("DateCierreConfig", DateCierreConfig);
-      }
-    });
-  }
 
-  return NextResponse.json(
-    { body: "Todo Naice señor" },
-    {
-      status: 200,
+        if (DateCierrePeriodo?.GruposCerrar?.length) {
+          const DateCierreConfig = await fetch(
+            `${process.env.NEXT_PUBLIC_APP_URL}/api/CierrePeriodo/Students`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                colegio: colegio,
+                grupos: DateCierrePeriodo?.GruposCerrar || [],
+              }),
+            }
+          ).then((res) => res?.json());
+
+          const DateCierreConfigDcne = await fetch(
+            `${process.env.NEXT_PUBLIC_APP_URL}/api/CierrePeriodo/Docentes`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                colegio: colegio,
+                grupos: DateCierrePeriodo?.GruposCerrar || [],
+              }),
+            }
+          ).then((res) => res?.json());
+        }
+      });
     }
-  );
+
+    return NextResponse.json(
+      { body: "Todo Naice señor" },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { body: "Error al consultar la información" },
+      { status: 404 }
+    );
+  }
 }
