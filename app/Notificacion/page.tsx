06c2ee927c77629946ef94b0c1f91c013b8f9e9f
ivanchohe90 <ivanchohe90@gmail.com>
2023-04-01 10:31:30 -0500

    Creacion de modal notificaciones

---
+++ app/Notificacion/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BodyComponent from "./BodyComponent";
+
+const Notificacion = () => {
+  return (
+    <>
+      <BodyComponent />
+    </>
+  );
+};
+
+export default Notificacion;
