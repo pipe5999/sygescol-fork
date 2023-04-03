1d099802aae3db040e23618de2ec2d253b92c66b
ivanchohe90 <ivanchohe90@gmail.com>
2023-04-03 17:01:18 -0500

    Reestructuracion en la firma del PDF I.E. SAGRADA FAMILIA

--- app/PDF/Preescolar/Estructura/Firmas.tsx
+++ app/PDF/Preescolar/Estructura/Firmas.tsx
@@ -7,18 +7,22 @@ const Firmas = ({ firma }: Props) => {
   return (
     <>
       <View style={{ width: "100%" }}>
-        {(firma && (
-          <Text style={{ margin: "auto" }}>_________________</Text>
-        )) || <Text style={{ margin: "auto" }}>_________________</Text>}
-        <Text style={{ margin: "auto", fontSize: 12, marginTop: 3 }}>
+        <View style={{ marginHorizontal: "30%", marginTop: -30 }}>
+          {firma && <Image style={{ width: 200 }} src={firma || ""} />}
+        </View>
+        {/* {(firma && <Text style={{ margin: "1%" }}>_________________</Text>) || (
+          <Text style={{ margin: "10%" }}>_________________</Text>
+        )} */}
+
+        <Text style={{ margin: "auto", fontSize: 12, marginTop: -15 }}>
           {JSON.parse(localStorage?.datosUsu)?.nombre}
         </Text>
         <Text style={{ margin: "auto", fontSize: 12 }}>
           Director/a de Grupo
         </Text>
-        <View style={{ marginHorizontal: "auto", border: 2 }}>
+        {/* <View style={{ marginHorizontal: "auto", border: 2 }}>
           {firma && <Image style={{ width: 150 }} src={firma || ""} />}
-        </View>
+        </View> */}
       </View>
     </>
   );
