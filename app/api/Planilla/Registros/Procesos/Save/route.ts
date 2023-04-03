23c38f7afd2cd1ca0cdb96c83fc544cebc19a8a9
brahian351 <brahiangamer@gmail.com>
2023-04-03 10:11:05 -0500

    cambios consulta nota

--- app/api/Planilla/Registros/Procesos/Save/route.ts
+++ app/api/Planilla/Registros/Procesos/Save/route.ts
@@ -1,31 +1,47 @@
 import { NextResponse } from "next/server";
 import { conecctions } from "../../../../../../utils/Conexions";
 
-export async function GET(req:any){
-    const {searchParams} = req.nextUrl;
-    let colegio = searchParams.get("c")
-    let proceso = searchParams.get("p")
-    let estudiante = searchParams.get("e")
-    let correcto = 0;
-    try {
-        
-        const conexion = conecctions[colegio]
-        const [procesoFind]:any =  await conexion.query(`SELECT * FROM newProcesosEvaluacion WHERE id = ${proceso}`)
-        const [search]: any = await conexion.query(`SELECT * FROM newProcesoEstudiante NPE INNER JOIN newProcesosEvaluacion PE ON NPE.proceso = PE.id WHERE estudiante = ${estudiante}`)
-        search?.map((pros:any) => {
-            console.log(`${pros?.escala} != ${procesoFind[0]?.escala}`)
-            if(pros?.escala != procesoFind[0]?.escala){
-                correcto++;
-            }
-        })
-        if(correcto == 0){
-            const [save]:any = await  conexion.query(`INSERT INTO newProcesoEstudiante(estudiante,proceso,periodo,cga) VALUES('${estudiante}','${proceso}','${procesoFind[0]?.periodo}','${procesoFind[0]?.cga}')`)
-            return NextResponse.json({body: "Información Guardada con Exito"},{status:200})
-        }else if(correcto > 0){
-            return NextResponse.json({body: "El estudiante ya tiene asignado procesos para otro desempeño"}, {status:200})
-        }
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({body: "Error al guardar la información"},{status:400})
+export async function GET(req: any) {
+  const { searchParams } = req.nextUrl;
+  let colegio = searchParams.get("c");
+  let proceso = searchParams.get("p");
+  let estudiante = searchParams.get("e");
+  let correcto = 0;
+  try {
+    const conexion = conecctions[colegio];
+    const [procesoFind]: any = await conexion.query(
+      `SELECT * FROM newProcesosEvaluacion WHERE id = ${proceso}`
+    );
+    const [search]: any = await conexion.query(
+      `SELECT * FROM newProcesoEstudiante NPE INNER JOIN newProcesosEvaluacion PE ON NPE.proceso = PE.id WHERE estudiante = ${estudiante} AND NPE.cga = ${procesoFind[0]?.cga}`
+    );
+    search?.map((pros: any) => {
+      console.log(`${pros?.escala} != ${procesoFind[0]?.escala}`);
+      if (pros?.escala != procesoFind[0]?.escala) {
+        correcto++;
+      }
+    });
+    if (correcto == 0) {
+      const [save]: any = await conexion.query(
+        `INSERT INTO newProcesoEstudiante(estudiante,proceso,periodo,cga) VALUES('${estudiante}','${proceso}','${procesoFind[0]?.periodo}','${procesoFind[0]?.cga}')`
+      );
+      return NextResponse.json(
+        { body: "Información Guardada con Exito" },
+        { status: 200 }
+      );
+    } else if (correcto > 0) {
+      return NextResponse.json(
+        {
+          body: "El estudiante ya tiene asignado procesos para otro desempeño",
+        },
+        { status: 200 }
+      );
     }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { body: "Error al guardar la información" },
+      { status: 400 }
+    );
+  }
 }
